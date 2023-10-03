82a031749b0c0c001270dbf24129a47b6ea109ef
vincenttuan <vincentjava@yahoo.com.tw>
2023-10-03 20:44:14 +0800

    java

--- JavaCryptography/src/security/AESSample.java
+++ JavaCryptography/src/security/AESSample.java
@@ -27,7 +27,16 @@ public class AESSample {
 		// 進行資料加密(將明文進行 AES-EBC 加密)
 		byte[] encryptedECB = KeyUtil.encryptWithAESKey(aesKeySpec, originalTest);
 		System.out.println("加密後的訊息:" + Arrays.toString(encryptedECB));
+		// byte[] 轉 base64 字串
 		System.out.println("加密後的訊息(Base64):" + Base64.getEncoder().encodeToString(encryptedECB));
+		// 進行解密
+		String base64 = "jK0VRYQnjX3y3/ZzyDNxQyjQyv5he2d+03VDd37Fmkl49flzn6mdTCCUu4kwgMsp";
+		// 將 base64 字串轉回 byte[]
+		byte[] encryptedECB_2 = Base64.getDecoder().decode(base64);
+		// 進行解密
+		String decryptedECB = KeyUtil.decryptWithAESKey(aesKeySpec, encryptedECB_2);
+		System.out.println("解密後的訊息:" + decryptedECB);
+		
 		// CBC 模式
 		
 		// CTR 模式

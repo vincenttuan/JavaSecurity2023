b55415c55c71e0123dc816bee9dc3198507bd258
vincenttuan <vincentjava@yahoo.com.tw>
2023-10-03 22:01:41 +0800

    AES

--- JavaCryptography/src/security/AESSample.java
+++ JavaCryptography/src/security/AESSample.java
@@ -8,7 +8,11 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class AESSample {
 	// 建立一個 AES Key (AES-128 bits, 16 bytes)
-	private static final String KEY = "0123456789abcdef"; // 16個字
+	//private static final String KEY = "0123456789abcdef"; // 16個字
+	// 建立一個 AES Key (AES-192 bits, 24 bytes)
+	//private static final String KEY = "0123456789abcdef12345678"; // 24個字
+	// 建立一個 AES Key (AES-256 bits, 32 bytes)
+	private static final String KEY = "0123456789abcdef0123456789abcdef"; // 32個字
 	// Random (透過時序為亂數種子，易被破解) 與 SecureRandom
 	private static final SecureRandom SECURE_RANDOM = new SecureRandom();
 	
@@ -30,7 +34,7 @@ public class AESSample {
 		// byte[] 轉 base64 字串
 		System.out.println("加密後的訊息(Base64):" + Base64.getEncoder().encodeToString(encryptedECB));
 		// 進行解密
-		String base64 = "viPRerPf6RVeQuHPD/732w==";
+		String base64 = "LyZu9u0Trqjupx09zgxYnA==";
 		// 將 base64 字串轉回 byte[]
 		byte[] encryptedECB_2 = Base64.getDecoder().decode(base64);
 		// 進行解密

7ae3dd4b3905ce8926ab06c2b90c5ba0b35145c8
vincenttuan <vincentjava@yahoo.com.tw>
2023-10-05 20:40:53 +0800

    java

--- JavaCryptography/src/security/keys/RSAChunkedEncryption.java
+++ JavaCryptography/src/security/keys/RSAChunkedEncryption.java
@@ -5,8 +5,6 @@ import java.util.Base64;
 
 import security.KeyUtil;
 
-import static security.KeyUtil.*;
-
 /**
  * 混合使用 RSA 分段加密技術。
  * 
@@ -24,23 +22,23 @@ public class RSAChunkedEncryption {
 
     public static void main(String[] args) throws Exception {
         // 初始化 Alice 和 Bob 的鑰匙對
-        KeyPair aliceKeyPair = generateRSAKeyPair();
-        KeyPair bobKeyPair = generateRSAKeyPair();
+        KeyPair aliceKeyPair = KeyUtil.generateRSAKeyPair();
+        KeyPair bobKeyPair = KeyUtil.generateRSAKeyPair();
 
         // Alice 的訊息
         String message = "Hello, Bob! 這是一個長訊息並使用 RSA 分段加密技術";
 
         // Alice 使用自己的私鑰對訊息進行數字簽名
-        byte[] signature = signMessage(aliceKeyPair.getPrivate(), message);
+        byte[] signature = KeyUtil.signMessage(aliceKeyPair.getPrivate(), message);
         String combinedMessage = message + "|" + Base64.getEncoder().encodeToString(signature);
 
         // Alice 使用 Bob 的公鑰對合併後的訊息進行分段RSA加密
-        byte[] encryptedMessage = encryptWithPublicKey(bobKeyPair.getPublic(), combinedMessage.getBytes());
+        byte[] encryptedMessage = KeyUtil.encryptWithPublicKey(bobKeyPair.getPublic(), combinedMessage.getBytes());
 
         // 這裡模擬 Alice 發送加密後的訊息到 Bob
 
         // Bob 使用他的私鑰對加密的訊息進行分段RSA解密
-        byte[] decryptedMessageBytes = decryptWithPrivateKey(bobKeyPair.getPrivate(), encryptedMessage);
+        byte[] decryptedMessageBytes = KeyUtil.decryptWithPrivateKey(bobKeyPair.getPrivate(), encryptedMessage);
         String decryptedCombinedMessage = new String(decryptedMessageBytes);
         // 1. 正則表示式 "|" 若要跳脫需要寫成 "\|"  
         // 2. 但是因為 "\" 也是 Java 的關鍵字所以要再跳脫變成 "\\|"

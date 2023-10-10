d45d86c667aa0c52f44a14d2a621dbd128207599
vincenttuan <vincentjava@yahoo.com.tw>
2023-10-10 19:23:00 +0800

    java

--- JavaCryptography/src/security/mac/MACExample.java
+++ JavaCryptography/src/security/mac/MACExample.java
@@ -1,5 +1,7 @@
 package security.mac;
 
+import java.util.Arrays;
+
 import javax.crypto.SecretKey;
 
 import security.KeyUtil;
@@ -9,7 +11,7 @@ public class MACExample {
 
 	public static void main(String[] args) throws Exception {
 		// 1. 定義我們要加上MAC的訊息
-		String message = "這是我的訊息";
+		String message = "以色列被攻擊...";
 		
 		// 2. 產生一把專用於 HMAC 的密鑰
 		SecretKey macKey = KeyUtil.generateKeyForHmac();
@@ -19,7 +21,17 @@ public class MACExample {
 		
 		// 4. 將 MAC 值印出
 		System.out.println("產生的 MAC: " + KeyUtil.bytesToHex(macValue));
-
+		
+		// 5. 在實際應用中，接收方會收到訊息(message)與 macValue
+		//    此時，message 要與 macKey(雙方統一都有的密鑰) 所產生的 computedMacValue 值進行與 macValue 的比對
+		byte[] computedMacValue = KeyUtil.generateMac("HmacSHA256", macKey, message.getBytes());
+		
+		// 6. 比較 macValue 與 computedMac 是否相同
+		if(Arrays.equals(macValue, computedMacValue)) {
+			System.out.println("MAC 驗證成功，來源正確");
+		} else {
+			System.out.println("MAC 驗證失敗");
+		}
 	}
 
 }

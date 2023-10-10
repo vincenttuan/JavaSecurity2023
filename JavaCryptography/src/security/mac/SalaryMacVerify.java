7b30c9aeb77590d89717b339ce8b2ef9944f92a4
vincenttuan <vincentjava@yahoo.com.tw>
2023-10-10 19:42:59 +0800

    java

---
+++ JavaCryptography/src/security/mac/SalaryMacVerify.java
@@ -0,0 +1,21 @@
+package security.mac;
+
+/*
+ * 
+ * 用於驗證薪資明細的 MAC
+ * 
+ * 證明成功實施了 MAC 驗證策略，並且可以正確地驗證您的薪資明細的完整性和來源。
+ * 使用這樣的策略可以確保只有擁有正確 MAC 密鑰的人（在這個例子中是 HR 部門）
+ * 才能生成有效的 MAC，而其他人則不能。
+ * 
+ * 這是一個非常重要的安全策略，特別是在涉及敏感資訊（如薪資明細）的場合。
+ * 只要保護好您的密鑰，就可以確保消息的真實性和完整性。
+*/
+public class SalaryMacVerify {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

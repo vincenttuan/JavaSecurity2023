cb2c2790f2bed035f905dc130aeb51326c93d94b
vincenttuan <vincentjava@yahoo.com.tw>
2023-10-19 20:18:30 +0800

    java

---
+++ JavaSecurityWeb/src/main/java/servlet/secure/form/LoginServlet.java
@@ -0,0 +1,27 @@
+package servlet.secure.form;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value = "/secure/servlet/form/login")
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		
+		PrintWriter out = resp.getWriter();
+		out.println("username：" + username);
+		out.println("password：" + password);
+		
+		
+	}
+	
+}

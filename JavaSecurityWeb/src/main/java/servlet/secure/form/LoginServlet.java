d58b3090592c706521eb223f4019e442943ac10a
vincenttuan <vincentjava@yahoo.com.tw>
2023-10-19 20:27:34 +0800

    java

--- JavaSecurityWeb/src/main/java/servlet/secure/form/LoginServlet.java
+++ JavaSecurityWeb/src/main/java/servlet/secure/form/LoginServlet.java
@@ -14,6 +14,10 @@ public class LoginServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/plain;charset=UTF-8"); // "text/html;charset=UTF-8"
+		
 		String username = req.getParameter("username");
 		String password = req.getParameter("password");
 		

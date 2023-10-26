5c6b73df55156de8f109e598500dbe4bfa7cb9f1
vincenttuan <vincentjava@yahoo.com.tw>
2023-10-26 21:39:34 +0800

    java

--- JavaSecurityWeb/src/main/java/servlet/secure/callback/OIDCCallback.java
+++ JavaSecurityWeb/src/main/java/servlet/secure/callback/OIDCCallback.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import servlet.secure.oidc.OIDCUtil;
+
 /**
  * 此 Servlet 處理從 Google OAuth 2.0 服務重定向回的 OAuth 2.0 回調。
  * 它將解析授權碼，獲取ID令牌，然後驗證令牌的有效性。
@@ -25,9 +27,18 @@ public class OIDCCallback extends HttpServlet {
 		resp.getWriter().print("<a href='/JavaSecurityWeb/form/login.jsp'>重新登入</a>");
 		
 		resp.getWriter().println("<pre>");
+		// 取得授權碼
 		String code = req.getParameter("code");
 		resp.getWriter().println("code: " + code);
-		
+		try {
+			// 得到 idToken
+			String idToken = OIDCUtil.getIDToken(code);
+			resp.getWriter().println("idToken: " + idToken); // JWT 的格式
+			
+			
+		} catch(Exception e) {
+			resp.getWriter().println("Exception: " + e);
+		}
 		
 		resp.getWriter().println("</pre>");
 	}

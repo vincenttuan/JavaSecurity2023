204adbd21c7c5af7aaccceda6b7f8a68758189ae
vincenttuan <vincentjava@yahoo.com.tw>
2023-10-26 21:27:33 +0800

    java

--- JavaSecurityWeb/src/main/java/servlet/secure/callback/OIDCCallback.java
+++ JavaSecurityWeb/src/main/java/servlet/secure/callback/OIDCCallback.java
@@ -1,5 +1,27 @@
 package servlet.secure.callback;
 
-public class OIDCCallback {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 此 Servlet 處理從 Google OAuth 2.0 服務重定向回的 OAuth 2.0 回調。
+ * 它將解析授權碼，獲取ID令牌，然後驗證令牌的有效性。
+ * 如果令牌有效，它將從ID令牌中提取用戶信息，並將令牌發送到報告服務。
+ */
+@WebServlet("/secure/callback/oidc")
+public class OIDCCallback extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String code = req.getParameter("code");
+		resp.getWriter().println("code: " + code);
+		
+	}
+	
 }
